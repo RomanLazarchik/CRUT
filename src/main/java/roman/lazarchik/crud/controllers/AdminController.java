09842ee237de1a03f79f50592b6548735af67738
Roman <rlazarchik@gmail.com>
2023-08-10 09:36:09 +0200

    first commit

---
+++ src/main/java/roman/lazarchik/crud/controllers/AdminController.java
@@ -0,0 +1,30 @@
+package roman.lazarchik.crud.controllers;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.*;
+import roman.lazarchik.crud.dao.PersonDAO;
+import roman.lazarchik.crud.models.Person;
+
+@Controller
+@RequestMapping("/admin")
+public class AdminController {
+
+    private final PersonDAO personDAO;
+    @Autowired
+    public AdminController(PersonDAO personDAO) {
+        this.personDAO = personDAO;
+    }
+@GetMapping
+    public String adminPage(Model model, @ModelAttribute("person")Person person){
+        model.addAttribute("people", personDAO.index());
+        return "adminPage";
+    }
+
+    @PatchMapping("/add")
+    public String mageAdmin(@ModelAttribute("person")Person person){
+        System.out.println(person.getId());
+        return "redirect:/people";
+    }
+}
